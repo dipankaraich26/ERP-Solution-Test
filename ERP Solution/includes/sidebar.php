f248f88422b37e24234979242033c884f3ff5c3b
Crimson9952 <crimson9952rb@gmail.com>
2025-12-30 22:36:11 +0530

    Fixed CSS

--- ERP Solution/includes/sidebar.php
+++ ERP Solution/includes/sidebar.php
@@ -11,12 +11,12 @@ $current = basename($_SERVER['PHP_SELF']);
     </button>
 
     <h2>ERP</h2>
-    <a href="/erp/part_master/list.php">Part Master</a>
-    <a href="/erp/purchase/index.php">Purchase</a>
-    <a href="/erp/depletion/index.php">Depletion</a>
-    <a href="/erp/inventory/index.php">Inventory</a>
-    <a href="/erp/reports/monthly.php">Reports</a>
-    <a href="/erp/suppliers/index.php">Suppliers</a>
-    <a href="/erp/bom/index.php">Bill Of Materials</a>
+    <a href="/erp/ERP Solution/part_master/list.php">Part Master</a>
+    <a href="/erp/ERP Solution/purchase/index.php">Purchase</a>
+    <a href="/erp/ERP Solution/depletion/index.php">Depletion</a>
+    <a href="/erp/ERP Solution/inventory/index.php">Inventory</a>
+    <a href="/erp/ERP Solution/reports/monthly.php">Reports</a>
+    <a href="/erp/ERP Solution/suppliers/index.php">Suppliers</a>
+    <a href="/erp/ERP Solution/bom/index.php">Bill Of Materials</a>
 </div>
 
