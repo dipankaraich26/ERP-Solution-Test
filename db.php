0057211e437127b11e35a54a562cf178a24e1987
RENwasTakenwasTaken <aryanaich008@gmail.com>
2026-02-04 00:39:43 +0530

    Added db credentials file for tracking and updated the coe.

--- db.php
+++ db.php
@@ -9,7 +9,7 @@ $pass = "erp4236$@#^";
 $port = "3306";
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("DB Error: " . $e->getMessage());
