e34580ca16bccc2811fe128d7bf61bf00546924c
Yashka-contacts <yashkacontacts@gmail.com>
2026-02-08 21:12:09 +0530

    y

--- hr/icons/icon.php
+++ hr/icons/icon.php
@@ -1,47 +1,47 @@
 <?php
 // Dynamic icon generator
 $size = isset($_GET['size']) ? (int)$_GET['size'] : 192;
-$size = min(max($size, 16), 512); // Clamp between 16 and 512
+$size = min(max($size, 16), 512);
 
 // Create image
 $image = imagecreatetruecolor($size, $size);
 
 // Colors
-$purple1 = imagecolorallocate($image, 102, 126, 234); // #667eea
-$purple2 = imagecolorallocate($image, 118, 75, 162);  // #764ba2
+$purple1 = imagecolorallocate($image, 102, 126, 234);
+$purple2 = imagecolorallocate($image, 118, 75, 162);
 $white = imagecolorallocate($image, 255, 255, 255);
 
-// Fill background with gradient-like effect
+// Fill background with gradient
 for ($y = 0; $y < $size; $y++) {
     $ratio = $y / $size;
     $r = (int)(102 + (118 - 102) * $ratio);
     $g = (int)(126 + (75 - 126) * $ratio);
     $b = (int)(234 + (162 - 234) * $ratio);
     $color = imagecolorallocate($image, $r, $g, $b);
-    imageline($image, 0, $y, $size, $y, $color);
+    imageline($image, 0, $y, $size - 1, $y, $color);
 }
 
 // Draw a simple clock/checkmark icon
-$centerX = $size / 2;
-$centerY = $size / 2;
-$radius = $size * 0.35;
+$centerX = (int)($size / 2);
+$centerY = (int)($size / 2);
+$radius = (int)($size * 0.35);
 
 // Draw circle (clock face)
-imagesetthickness($image, max(2, $size / 40));
+imagesetthickness($image, max(2, (int)($size / 40)));
 imageellipse($image, $centerX, $centerY, $radius * 2, $radius * 2, $white);
-imagefilledellipse($image, $centerX, $centerY, $radius * 1.8, $radius * 1.8, $white);
+imagefilledellipse($image, $centerX, $centerY, (int)($radius * 1.8), (int)($radius * 1.8), $white);
 
 // Draw checkmark inside
 $checkColor = $purple1;
-$thickness = max(3, $size / 25);
+$thickness = max(3, (int)($size / 25));
 imagesetthickness($image, $thickness);
 
-$checkStartX = $centerX - $radius * 0.4;
+$checkStartX = (int)($centerX - $radius * 0.4);
 $checkStartY = $centerY;
-$checkMidX = $centerX - $radius * 0.1;
-$checkMidY = $centerY + $radius * 0.3;
-$checkEndX = $centerX + $radius * 0.5;
-$checkEndY = $centerY - $radius * 0.3;
+$checkMidX = (int)($centerX - $radius * 0.1);
+$checkMidY = (int)($centerY + $radius * 0.3);
+$checkEndX = (int)($centerX + $radius * 0.5);
+$checkEndY = (int)($centerY - $radius * 0.3);
 
 imageline($image, $checkStartX, $checkStartY, $checkMidX, $checkMidY, $checkColor);
 imageline($image, $checkMidX, $checkMidY, $checkEndX, $checkEndY, $checkColor);
